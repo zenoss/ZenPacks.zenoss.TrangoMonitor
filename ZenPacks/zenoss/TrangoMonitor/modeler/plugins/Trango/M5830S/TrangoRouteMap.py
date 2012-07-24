fa73106ca2262f76a863a342813864d95f2c4e6f
gstreiff <gstreiff@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-07-24 05:24:22 +0000

    [trunk/enterprise] update source headers per legal for 4.2.x and beyond using Summer's script fu; reviewed by gstreiff jhanson
    git-svn-id: http://dev.zenoss.com/svnint/trunk/enterprise/zenpacks/ZenPacks.zenoss.TrangoMonitor@61802 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- ZenPacks/zenoss/TrangoMonitor/modeler/plugins/Trango/M5830S/TrangoRouteMap.py
+++ ZenPacks/zenoss/TrangoMonitor/modeler/plugins/Trango/M5830S/TrangoRouteMap.py
@@ -1,8 +1,12 @@
-######################################################################
-#
-# Copyright 2008 Zenoss, Inc.  All Rights Reserved.
-#
-######################################################################
+##############################################################################
+# 
+# Copyright (C) Zenoss, Inc. 2008, all rights reserved.
+# 
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+# 
+##############################################################################
+
 
 __doc__ = """TrangoRouteMap
 Gather routing information from Trango M583OS devices.
