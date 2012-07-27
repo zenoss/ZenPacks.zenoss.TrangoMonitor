dcf50ffff214632e8644649e03f92f2a1d352f88
bedwards <bedwards@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-07-27 21:13:08 +0000

    Fixes ZEN-2725. Reviewed by Danny. Add marker egg to detect version.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/enterprise/zenpacks/ZenPacks.zenoss.TrangoMonitor@62064 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ AUTHOR = "Zenoss"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
 PACKAGES = ['ZenPacks', 'ZenPacks.zenoss', 'ZenPacks.zenoss.TrangoMonitor']
-INSTALL_REQUIRES = []
+INSTALL_REQUIRES = ['Zenoss >= 2.4']
 COMPAT_ZENOSS_VERS = ">= 2.4"
 PREV_ZENPACK_NAME = ""
 # STOP_REPLACEMENTS
